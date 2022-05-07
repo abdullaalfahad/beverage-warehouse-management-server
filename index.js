f42656f29a35d45fc4b75b5fa7f51880fb646e2e
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-08 00:09:01 +0600

    delete api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -37,6 +37,13 @@ async function run() {
         app.post('/inventory', async (req, res) => {
             const contact = req.body;
             const result = await inventoryCollection.insertOne(contact);
+            res.send(result);
+        })
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await inventoryCollection.deleteOne(query);
             res.send(result);
         })
     }
