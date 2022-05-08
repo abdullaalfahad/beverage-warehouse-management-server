43c1d5ce3555087929a741670a4900a152aa841f
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-08 16:00:55 +0600

    update api add

--- index.js
+++ index.js
@@ -53,6 +53,20 @@ async function run() {
             const result = await inventoryCollection.deleteOne(query);
             res.send(result);
         })
+
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updated = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updated.quantity
+                }
+            }
+            const result = await inventoryCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
     }
     finally {
 
